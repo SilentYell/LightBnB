71ffcbb4e976bad51c391c47149936de47b5f7b2
Nik Hixson <nikilobe@gmail.com>
2024-12-20 13:19:48 -0800

    Implemented addProperty function to insert properties into the database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -136,7 +136,7 @@ const getAllProperties = function (options, limit = 10) {
 
   // 2. Start the query
   let queryString = `
-    SELECT properties.*, avg(property_reviews.rating) as average_rating,
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
     LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
   `;
@@ -207,10 +207,41 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties (
+      owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+      cost_per_night, street, city, province, post_code, country,
+      parking_spaces, number_of_bathrooms, number_of_bedrooms
+    ) VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+    )
+    RETURNING *;
+  `;
+
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    Math.round(property.cost_per_night * 100), // Convert to cents and round to avoid floating-point issues
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.error('Error executing query', err.stack);
+      throw err;
+    });
 };
 
 module.exports = {
